e006d6925ce38f73747208070e00507fc0069977
Cofiee <Marvorginal@gmail.com>
2023-06-03 19:39:17 +0200

    cos troche dziala

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import PySimpleGUI as sg
 import numpy as np
 
+# Nb liczba kolumn stanu to 4
+# Nk długość klucza w 32 bitowych słowach dla 128bit długość to 4
+# Nr liczba rund szyfrowania dla klucza 128bit to 10 (128 -> 10, 192 -> 12, 256 -> 14)
+
 sbox = [
             0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
             0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -19,12 +23,33 @@ sbox = [
             0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
             0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
         ]
+inv_sbox = []
+galois28 = [
+            0x00, 0x02, 0x04, 0x06, 0x08, 0x0a, 0x0c, 0x0e, 0x10, 0x12, 0x14, 0x16, 0x18, 0x1a, 0x1c, 0x1e,
+            0x20, 0x22, 0x24, 0x26, 0x28, 0x2a, 0x2c, 0x2e, 0x30, 0x32, 0x34, 0x36, 0x38, 0x3a, 0x3c, 0x3e,
+            0x40, 0x42, 0x44, 0x46, 0x48, 0x4a, 0x4c, 0x4e, 0x50, 0x52, 0x54, 0x56, 0x58, 0x5a, 0x5c, 0x5e,
+            0x60, 0x62, 0x64, 0x66, 0x68, 0x6a, 0x6c, 0x6e, 0x70, 0x72, 0x74, 0x76, 0x78, 0x7a, 0x7c, 0x7e,
+            0x80, 0x82, 0x84, 0x86, 0x88, 0x8a, 0x8c, 0x8e, 0x90, 0x92, 0x94, 0x96, 0x98, 0x9a, 0x9c, 0x9e,
+            0xa0, 0xa2, 0xa4, 0xa6, 0xa8, 0xaa, 0xac, 0xae, 0xb0, 0xb2, 0xb4, 0xb6, 0xb8, 0xba, 0xbc, 0xbe,
+            0xc0, 0xc2, 0xc4, 0xc6, 0xc8, 0xca, 0xcc, 0xce, 0xd0, 0xd2, 0xd4, 0xd6, 0xd8, 0xda, 0xdc, 0xde,
+            0xe0, 0xe2, 0xe4, 0xe6, 0xe8, 0xea, 0xec, 0xee, 0xf0, 0xf2, 0xf4, 0xf6, 0xf8, 0xfa, 0xfc, 0xfe,
+            0x1b, 0x19, 0x1f, 0x1d, 0x13, 0x11, 0x17, 0x15, 0x0b, 0x09, 0x0f, 0x0d, 0x03, 0x01, 0x07, 0x05,
+            0x3b, 0x39, 0x3f, 0x3d, 0x33, 0x31, 0x37, 0x35, 0x2b, 0x29, 0x2f, 0x2d, 0x23, 0x21, 0x27, 0x25,
+            0x5b, 0x59, 0x5f, 0x5d, 0x53, 0x51, 0x57, 0x55, 0x4b, 0x49, 0x4f, 0x4d, 0x43, 0x41, 0x47, 0x45,
+            0x7b, 0x79, 0x7f, 0x7d, 0x73, 0x71, 0x77, 0x75, 0x6b, 0x69, 0x6f, 0x6d, 0x63, 0x61, 0x67, 0x65,
+            0x9b, 0x99, 0x9f, 0x9d, 0x93, 0x91, 0x97, 0x95, 0x8b, 0x89, 0x8f, 0x8d, 0x83, 0x81, 0x87, 0x85,
+            0xbb, 0xb9, 0xbf, 0xbd, 0xb3, 0xb1, 0xb7, 0xb5, 0xab, 0xa9, 0xaf, 0xad, 0xa3, 0xa1, 0xa7, 0xa5,
+            0xdb, 0xd9, 0xdf, 0xdd, 0xd3, 0xd1, 0xd7, 0xd5, 0xcb, 0xc9, 0xcf, 0xcd, 0xc3, 0xc1, 0xc7, 0xc5,
+            0xfb, 0xf9, 0xff, 0xfd, 0xf3, 0xf1, 0xf7, 0xf5, 0xeb, 0xe9, 0xef, 0xed, 0xe3, 0xe1, 0xe7, 0xe5
+            ]
+rcon = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36]
 
 def main():
     layout = [
-        [sg.Text("klucz")],
-        [sg.Text("wiadomość")],
-        [sg.Text("szyfrogram")],
+        [sg.Text("klucz:"), sg.InputText(key='key64')],
+        [sg.Text("wiadomość:"), sg.Multiline(size=(30,5), key='message')],
+        [sg.Text("szyfrogram:")],
+        [sg.Multiline(size=(30,5), key='output', disabled=True)],
         [sg.Button("Szyfruj"), sg.Button("Wyczyść")]
     ]
 
@@ -33,37 +58,194 @@ def main():
     while True:
         event, values = window.read()
         if event == "Szyfruj":
-            encrypt()
+            encrypt("", "")
         elif event == "Wyczyśc":
             pass
         elif event == sg.WIN_CLOSED:
             break
 
 
-def encrypt(message: str) -> str:
-    pass
+def encrypt(message: str, key: str) -> str:
+    # debug
+    message = "3243F6A8885A308D313198A2E0370734"
+    key = "2B7E151628AED2A6ABF7158809CF4F3C"
+
+    state = initialize_state(message)
+    print("Initialize matrix: ")
+    print([hex(i) for i in state])
+
+    key_schedule = key_expansion(key)
+    print("Initial key: ")
+    print([hex(i) for i in key_schedule[0]])
+
+    state = add_round_key(state, key_schedule[0])
+    print("After key addition: ")
+    print([hex(i) for i in state])
+
+    for i in range(1, 2):
+        state = byte_substitution(state)
+        print("After byte sub: ")
+        print([hex(i) for i in state])
+        state = shift_rows(state)
+        print("After row Shift: ")
+        print([hex(i) for i in state])
+        state = mix_columns(state)
+        print("After mix column: ")
+        print([hex(i) for i in state])
+        print("Round key: ")
+        print([hex(i) for i in key_schedule[i]])
+        state = add_round_key(state, key_schedule[i])
+        print("After key addition: ")
+        print([hex(i) for i in state])
+
+    state = byte_substitution(state)
+    print("After byte sub: ")
+    print([hex(i) for i in state])
+    state = shift_rows(state)
+    print("After row Shift: ")
+    print([hex(i) for i in state])
+    print("Round key: ")
+    print([hex(i) for i in key_schedule[i]])
+    state = add_round_key(state, key_schedule[10])
+    print("After key addition: ")
+    print([hex(i) for i in state])
+    return state
+
+# def decrypt(cipher):
+#     state = initialize_state(message)
+#     state = add_round_key(state, key)
+#     state = inv_shift_rows(state)
+#     state = inv_byte_substitution(state)
+#     for i in range(9, 0, -1):
+#         state = add_round_key(state, key)
+#         state = inv_mix_columns(state)
+#         state = inv_shift_rows(state)
+#         state = inv_byte_substitution(state)
+#
+#     state = add_round_key(state, key)
+#     return state
 
 
 def initialize_state(plain_text):
     state = []
     while plain_text:
-        state.append(plain_text[0:2])
-        state = plain_text[2:]
+        state.append(int(plain_text[0:2], 16))
+        plain_text = plain_text[2:]
     return state
 
 
 def rotation(arr):
     first = arr[0]
     arr = arr[1:]
-    return arr + first
+    arr.append(first)
+    return arr
 
 
 def xor(v1, v2):
     return v1 ^ v2
 
 
-def key_expansion():
-    pass
+def key_expansion(key) -> list:
+    #key_schedule = [int(c, 16) for c in key]
+    expanded_key = []
+    while key:
+        expanded_key.append(int(key[0:2], 16))
+        key = key[2:]
+
+    round_num = 4
+    tmp = [0]*4
+    while round_num < 44:
+        from_4_rounds_ago = expanded_key[4 * (round_num - 4): 4 * (round_num - 3)]
+        prev_round = expanded_key[4 * (round_num-1): 4 * round_num]
+        if round_num % 4 == 0:
+            #prev_round = sub(rotation(prev_round))
+            prev_round = byte_substitution(rotation(prev_round))
+            for i in range(0, 4):
+                tmp[i] = xor(from_4_rounds_ago[i], prev_round[i])
+            #tmp[0] = xor(tmp[0], rc[round_num // 4])
+            tmp[0] = xor(tmp[0], galois28[round_num // 4])
+        else:
+            for i in range(0, 4):
+                tmp[i] = xor(from_4_rounds_ago[i], prev_round[i])
+        expanded_key = expanded_key + tmp
+        round_num += 1
+    #return key_schedule
+    return [expanded_key[i: i+16] for i in range(0, len(expanded_key), 16)]
+
+
+def add_round_key(state, key_schedule) -> list:
+    new_state = []
+    for i in range(0, 16):
+        new_state.append(xor(key_schedule[i], state[i]))
+    return new_state
+
+
+def byte_substitution(state) -> list:
+    new_state = []
+    for byte in state:
+        new_state.append(sbox[byte])
+    return new_state
+
+def inv_byte_substitution(state) -> list:
+    new_state = []
+    for byte in state:
+        new_state.append(inv_sbox[byte])
+    return new_state
+
+
+def shift_rows(state) -> list:
+    new_state = [None] * 16
+    new_state[0] = state[0]
+    new_state[1] = state[5]
+    new_state[2] = state[10]
+    new_state[3] = state[15]
+    new_state[4] = state[4]
+    new_state[5] = state[9]
+    new_state[6] = state[14]
+    new_state[7] = state[3]
+    new_state[8] = state[8]
+    new_state[9] = state[13]
+    new_state[10] = state[2]
+    new_state[11] = state[7]
+    new_state[12] = state[12]
+    new_state[13] = state[1]
+    new_state[14] = state[6]
+    new_state[15] = state[11]
+    return new_state
+
+
+def mix_columns(state):
+    tmp = [0]*len(state)
+    for i in range(0, 4):
+        tmp_col = [state[i], state[4+i], state[8+i], state[12+i]]
+        tmp_col = mix_column(tmp_col)
+        tmp[i] = tmp_col[0]
+        tmp[4+i] = tmp_col[1]
+        tmp[8+i] = tmp_col[2]
+        tmp[12+i] = tmp_col[3]
+    return tmp
+
+
+def mix_column(col):
+    tmp_col = [0]*4
+    tmp_col[0] = mult(2, col[0]) ^ mult(3, col[1]) ^ col[2] ^ col[3]
+    tmp_col[1] = col[0] ^ mult(2, col[1]) ^ mult(3, col[2]) ^ col[3]
+    tmp_col[2] = col[0] ^ col[1] ^ mult(2, col[2]) ^ mult(3, col[3])
+    tmp_col[3] = mult(3, col[0]) ^ col[1] ^ col[2] ^ mult(2, col[3])
+    return tmp_col
+
+
+def mult(num1, num2):  # Galois zmienic na lookup
+    p = 0x00
+    for i in range(0, 8):
+        if num2 & 1 != 0:
+            p = p ^ num1
+        hi_bit_set = (num1 & 0x80) != 0
+        num1 = num1 << 1;
+        if hi_bit_set:
+            num1 = num1 ^ 0x1b # x^8 + x^4 + x^3 + x + 1
+        num2 = num2 >> 1
+    return p
 
 
 if __name__ == '__main__':
